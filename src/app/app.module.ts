2ce52c9afbcd93f2360650e76b1bd4af6833e4c1
Sone <sthaya@gmail.com>
2021-02-28 17:30:20 -0800

    created warning alert and success alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
+// import { ServerComponent } from './server/server.component';
+// import { ServersComponent } from './servers/servers.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { FormsModule } from '@angular/forms';
-import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    // ServersComponent,
+    // ServerComponent,
+    // ServersComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
